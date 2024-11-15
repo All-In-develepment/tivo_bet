02babd30a7fe42bd4d368dac5fcdddb2e7827567
ronaldocestrela <ronaldo@grupoall.in>
2024-11-15 11:15:12 -0300

    core: buscando resultados

--- tivobet.py
+++ tivobet.py
@@ -6,32 +6,19 @@ def main():
         # Inicia o navegador
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
-        page.goto("https://tivo.bet/br/sportsbook/virtual")
+        page.goto("https://vgpclive-vs001.akamaized.net/virtualsport/stable/dist/entryPoints/vwmfTSUOF.html?product=vwmf1&sport=vwmf&client_id=3586&language=br")
         
         # Acesse o primeiro iframe e clica no item desejado
-        page.wait_for_selector('iframe[name="sportsbook_iframe"]', timeout=15000)
-        iframe = page.frame(name="sportsbook_iframe")
+        page.wait_for_selector('div > button.results-page-button', timeout=15000)
+        page.locator('div > button.results-page-button').click()
         
-        # Verifica se o iframe foi encontrado
-        if iframe is None:
-            print("Erro: iframe não encontrado!")
-            browser.close()
-            return
-
-        # Aguarda o seletor dentro do iframe e tenta clicar nele
-        iframe.locator("#virtual-champ-select > ul:nth-child(1) > li:nth-child(4) > span").click()
-        
-        # Espera o segundo iframe carregar (se necessário)
-        iframe2_locator = page.frame_locator("iframe[name='virtualSportsIframe']")
-        iframe2 = iframe2_locator.locator("iframe").nth(0)  # Acessa o conteúdo do segundo iframe
-        
-        # Espera um pouco para garantir que o conteúdo do iframe esteja disponível
-        time.sleep(2)  # Você pode usar o método de espera do Playwright, como `page.wait_for_selector()`
-
-        iframe3_locator = iframe2.frame_locator('iframe[title="Virtual Sports iFrame"]')
-        iframe3 = iframe3_locator.locator("iframe").nth(0)
-        iframe3.locator ('div > button.results-page-button').click()
-       
+        # Pega primeiro time
+        total_partdas = 1
+        while total_partdas <= 10:
+            timeA = page.locator(f"#results-details > div.results > div > div > div > div > div:nth-child({total_partdas}) > div.home-name").inner_text()
+            print("Total de partidas: ", timeA)
+            total_partdas += 1
+        # time.sleep(5)
 
         # Fecha o navegador
         browser.close()
